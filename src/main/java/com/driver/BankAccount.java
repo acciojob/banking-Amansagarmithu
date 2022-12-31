8546ce55c20a40c3ee1402ca827f9f6156adeb20
Aman Sagar <amansagar887@gmail.com>
2022-12-31 09:49:38 +0530

    first

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -7,25 +7,62 @@ public class BankAccount {
     private double minBalance;
 
     public BankAccount(String name, double balance, double minBalance) {
+        this.name = name;
+        this.balance = balance;
+        this.minBalance = minBalance;
+    }
 
+    public BankAccount() {
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-
-        return null;
+        try{
+            int a = digits%10;
+            digits/=10;
+            int add = a+digits;
+            if(add==sum) return String.valueOf(digits*10+a);
+            else{
+                throw new Exception();
+            }
+        }
+        catch(Exception e){
+            return "Account Number can not be generated";
+        }
     }
 
     public void deposit(double amount) {
         //add amount to balance
+        this.balance +=amount;
 
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-
+        try{
+            if(this.balance<minBalance){
+                throw new Exception();
+            }
+            else{
+                this.balance-=amount;
+            }
+        }
+        catch(Exception e){
+            System.out.println("Insufficient Balance");
+        }
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
 }
